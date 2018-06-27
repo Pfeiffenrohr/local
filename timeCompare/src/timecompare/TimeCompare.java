398e957a3b0744bd4da5c1b89e628878d429e5ea
Richard Lechner <lechnerri@mediamarktsaturn.com>
2018-06-27 15:15:59 +0200

    Änderung

--- timeCompare/src/timecompare/TimeCompare.java
+++ timeCompare/src/timecompare/TimeCompare.java
@@ -5,7 +5,7 @@ import java.util.Calendar;
 import java.util.Date;
 
 public class TimeCompare {
-	 public static void main( String args[] ) {
+	 public static void main( String args[] ) { 
 		 
 		 if (args.length < 11 )
 		 {
