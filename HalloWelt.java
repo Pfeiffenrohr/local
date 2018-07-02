190959da65b37154d66150f27ca3b543b7e790e5
Richard Lechner <lechnerri@mediamarktsaturn.com>
2018-07-02 09:51:44 +0200

    HalloWelt

--- HalloWelt.java
+++ HalloWelt.java
@@ -2,7 +2,7 @@ public class HalloWelt {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Systeme.out.println("Hallo Welt");
+		System.out.println("Hallo Welt");
 
 	}
 
