caec2f32914a19a6b038e01e6c6181ac444f190a
Richard Lechner <lechnerri@mediamarktsaturn.com>
2018-08-10 23:48:15 +0200

    Neustes Base Snapshot

--- CBudgetBatch/scripts/plan_cache_trigger.sh
+++ CBudgetBatch/scripts/plan_cache_trigger.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 date >>/home/budget/budget/plan_cache_trigger.log
 cd /home/budget/budget
-/usr/bin/java -Xms512m  -Xmx1024m  -classpath /home/budget/budget/server.jar:/home/budget/budget/lib/CbudgetBase-0.0.2-SNAPSHOT.jar:/home/budget/budget/lib/postgresql-42.2.4.jar  cbudgetbatch.BerechnePlanungBatch budget budget budget trigger>> /home/budget/budget/plan_cache_trigger.log
+/usr/bin/java -Xms512m  -Xmx1024m  -classpath /home/budget/budget/server.jar:/home/budget/budget/lib/CbudgetBase-1.0.1-SNAPSHOT.jar:/home/budget/budget/lib/postgresql-42.2.4.jar  cbudgetbatch.BerechnePlanungBatch budget budget budget trigger>> /home/budget/budget/plan_cache_trigger.log
