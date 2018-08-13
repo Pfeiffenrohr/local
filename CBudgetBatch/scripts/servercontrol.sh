714018ac2ee1b366d5ef839919e63aff7880ee6c
Richard Lechner <lechnerri@mediamarktsaturn.com>
2018-08-13 08:39:02 +0200

    servercontrol angepasst

--- CBudgetBatch/scripts/servercontrol.sh
+++ CBudgetBatch/scripts/servercontrol.sh
@@ -2,6 +2,6 @@
 ps aux |grep budget_server|grep -v grep >/dev/null
 if [ $? -ne 0 ]; then
 host=`hostname`
-echo "start server on $host ..." >> /storage-pool/budget/log.txt
-/home/richard/budget/budget_server.sh &
+echo "start server on $host ..." >> /home/budget/budget/log.txt
+/home/budget/budget/budget_server.sh &
 fi
