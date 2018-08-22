49bc37854da99104230d2cc8f23cc222e3c2e79f
Richard Lechner <lechnerri@mediamarktsaturn.com>
2018-08-22 10:10:48 +0200

    demospring

--- demospring/src/main/java/de/richardlechner/topic/TopicService.java
+++ demospring/src/main/java/de/richardlechner/topic/TopicService.java
@@ -1,5 +1,6 @@
 package de.richardlechner.topic;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -7,14 +8,36 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class TopicService {
-	List <Topic> topics = Arrays.asList(
+	List <Topic> topics = new ArrayList<> ( Arrays.asList(
 			new Topic("1","eins", "desc eins"),
 			new Topic("2","zwei", "desc zwei"),
 			new Topic("3","drei", "desc drei")
-			);
+			));
 	
 	public List<Topic> getallTopics() {
 		return topics;
 	}
+	
+	public Topic getTopic(String id) {
+	return	topics.stream().filter( t -> t.getId().equals(id)).findFirst().get();
+	}
+	
+	public void addTopic(Topic topic) {
+		topics.add(topic);
+		}
+	
+	public void updateTopic(String id, Topic topic) {
+		for (int i=0; i< topics.size(); i++){
+			Topic t = topics.get(i);
+			if (t.getId().equals(id)) {
+				topics.set(i, topic);
+			}
+		}
+	}
+		public void deleteTopic(String id) {
+				topics.removeIf(t -> t.getId().equals(id)); 
+			
+		}
+	
 }	
 
