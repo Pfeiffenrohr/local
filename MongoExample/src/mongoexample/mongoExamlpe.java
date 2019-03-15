259f133b28e27b5cba622c88cd06daf613aa138e
lechnerri <lechnerri@DEV135NOB7364.hq-clients.hq.media-saturn.net>
2019-03-15 12:38:49 +0100

    Angepasst an Ucmp und treiber local

--- MongoExample/src/mongoexample/mongoExamlpe.java
+++ MongoExample/src/mongoexample/mongoExamlpe.java
@@ -2,13 +2,16 @@ package mongoexample;
 
 
 import java.net.UnknownHostException;
+import java.util.Arrays;
 import java.util.Date;
 import com.mongodb.BasicDBObject;
 import com.mongodb.DB;
 import com.mongodb.DBCollection;
 import com.mongodb.DBCursor;
 import com.mongodb.MongoClient;
+import com.mongodb.MongoCredential;
 import com.mongodb.MongoException;
+import com.mongodb.ServerAddress;
 import com.mongodb.WriteConcernError;
 //import com.mongodb.MongoException;
 
@@ -24,39 +27,42 @@ public class mongoExamlpe {
 	/**** Connect to MongoDB ****/
 	// Since 2.10.0, uses MongoClient
 	//MongoClient mongo = new MongoClient("ing04esbsand01", 27017);
-    	MongoClient mongo = new MongoClient("localhost", 27017);
+    	MongoCredential credential = MongoCredential.createCredential("ucmp-test", "ucmp-test", "Ehaneboke950".toCharArray());
+    	MongoClient mongo = new MongoClient(new ServerAddress("localhost", 27016), Arrays.asList(credential));
 	/**** Get database ****/
+    	
 	// if database doesn't exists, MongoDB will create it for you
-	DB db = mongo.getDB("testdb");
+	DB db = mongo.getDB("ucmp-test");
 
 	/**** Get collection / table from 'testdb' ****/
 	// if collection doesn't exists, MongoDB will create it for you
-	DBCollection table = db.getCollection("user");
+	DBCollection table = db.getCollection("Customer");
 	/**** Insert ****/
+	long anz = table.count();
+	System.out.println("Anz = " + anz);
 	// create a document to store key and value
 	
 	BasicDBObject document = new BasicDBObject();
-	document.put("name", "mkyong");
-	document.put("age", 30);
-	document.put("createdDate", new Date());
+	//document.put("name", "mkyong");
+	//document.put("age", 30);
+	//document.put("createdDate", new Date());
 	
 	//table.insert(document, writeConcern);
-	table.insert(document);
+	//table.insert(document);
 
 	/**** Find and display ****/
 	BasicDBObject searchQuery = new BasicDBObject();
-	searchQuery.put("name", "mkyong");
+	//searchQuery.put("name", "mkyong");
 
-	DBCursor cursor = table.find(searchQuery);
+	//DBCursor cursor = table.find(searchQuery);
 
-	while (cursor.hasNext()) {
-		System.out.println(cursor.next());
-	}
+	//while (cursor.hasNext()) {
+	//	System.out.println(cursor.next());
+	//}
 
 	/**** Update ****/
 	// search document where name="mkyong" and update it with new values
-	BasicDBObject query = new BasicDBObject();
-	query.put("name", "mkyong");
+/*	query.put("name", "mkyong");
 
 	BasicDBObject newDocument = new BasicDBObject();
 	newDocument.put("name", "mkyong-updated");
@@ -67,14 +73,14 @@ public class mongoExamlpe {
 	table.update(query, updateObj);
 
 	/**** Find and display ****/
-	BasicDBObject searchQuery2
+	/*BasicDBObject searchQuery2
 	    = new BasicDBObject().append("name", "mkyong-updated");
 
 	DBCursor cursor2 = table.find(searchQuery2);
 
 	while (cursor2.hasNext()) {
 		System.out.println(cursor2.next());
-	}
+	}*/
 
 	/**** Done ****/
 	System.out.println("Done");
