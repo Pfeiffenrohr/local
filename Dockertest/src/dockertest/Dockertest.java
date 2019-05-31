6bdb7ae253644aaa09085f42273a5462c08a8f49
lechnerri <lechnerri@media-saturn.com>
2019-05-31 09:53:56 +0200

    initial

---
+++ Dockertest/src/dockertest/Dockertest.java
@@ -0,0 +1,10 @@
+package dockertest;
+
+public class Dockertest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Dies ist ein Dockertest");
+	}
+
+}
